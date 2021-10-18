dc3f7ada6352ff579d288269346f01376d10cafd
Melsik1127 <Melsik1127@gmail.com>
2021-10-18 16:50:42 +0300

    s

--- start_files.py
+++ start_files.py
@@ -1,4 +1,4 @@
 import subprocess
 
-process1 = subprocess.Popen(["python", "design_bot/main.py"]) 
-process2 = subprocess.Popen(["python", "find_clients/run.py"])
+process1 = subprocess.Popen(["python3", "design_bot/main.py"]) 
+process2 = subprocess.Popen(["python3", "find_clients/run.py"])
